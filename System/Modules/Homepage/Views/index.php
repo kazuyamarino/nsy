79920c5df21d2e9a577fa18581c697ef273da34d
Vikry <kazuya_marino_4858@yahoo.co.id>
2018-10-17 15:09:21 +0700

    update several files

--- System/Modules/Homepage/Views/index.php
+++ System/Modules/Homepage/Views/index.php
@@ -6,7 +6,7 @@
 		<div class="large-12 cell">
 			<div class="callout large">
 				<h3>Hi, NSY Users!&nbsp;|&nbsp;<a href="<?php echo BASE_URL ?>">Go To MVC Page</a></h3>
-				<img src="<?php echo IMG_DIR ?>magic.gif" />
+				<p>This is HMVC page</p>
 			</div>
 		</div>
 	</div>
