b2b2dc820bc0bf841ed2fa5d782a400a2e821d47
kazuyamarino <vikry.yuansah@gmail.com>
2020-01-04 02:28:23 +0700

    update cli and script composer

--- INSTALL.sh
+++ INSTALL.sh
@@ -6,11 +6,9 @@ then
 	echo 'source ~/nsy.cli.sh' >> ~/.bashrc
 	echo 'test -f ~/.bashrc && . ~/.bashrc' >> ~/.bash_profile
 	echo 'test -f ~/.profile && . ~/.profile' >> ~/.bash_profile
-	source ~/.bashrc
 
 	printf "NSY CLI installed\n"
 else
 	cp .cli/nsy.cli.sh ~/nsy.cli.sh
-	source ~/.bashrc
 	printf "NSI CLI already installed and has been updated\n"
 fi
