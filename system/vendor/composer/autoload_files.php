eae0198ca5b87d91bc3eda964bc88e959cd80269
kazuyamarino <vikry.yuansah@gmail.com>
2020-01-16 08:58:34 +0700

    update autoload

--- system/vendor/composer/autoload_files.php
+++ system/vendor/composer/autoload_files.php
@@ -9,5 +9,4 @@ return array(
     '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
     '320cde22f66dd4f5d3fd621d3e88b98f' => $vendorDir . '/symfony/polyfill-ctype/bootstrap.php',
     '182b55b2b69e7e1b561ee34d36775279' => $baseDir . '/system/core/NSY_Helpers.php',
-    '9ac2559de43d0c35d23814e693a424d2' => $baseDir . '/system/helpers/Helpers_CI.php',
 );
--- system/vendor/composer/autoload_static.php
+++ system/vendor/composer/autoload_static.php
@@ -10,7 +10,6 @@ class ComposerStaticInityour_suffixed
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '182b55b2b69e7e1b561ee34d36775279' => __DIR__ . '/../../..' . '/system/core/NSY_Helpers.php',
-        '9ac2559de43d0c35d23814e693a424d2' => __DIR__ . '/../../..' . '/system/helpers/Helpers_CI.php',
     );
 
     public static $prefixLengthsPsr4 = array (
