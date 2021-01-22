41f304e4de33af84551d5b4f4c03daf2a094983c
kazuyamarino <vikry.yuansah@gmail.com>
2021-01-22 15:08:51 +0700

    fix text

--- system/modules/homepage/controllers/Hello.php
+++ system/modules/homepage/controllers/Hello.php
@@ -11,9 +11,9 @@ class Hello extends Load
 	public function index_hmvc()
 	{
 		$arr = [
-			'welcome' => Load::model('Model_Welcome')->welcome(), // Call my_name method from Model_Welcome
-			'hmvc_page' => Load::model('Homepage\Model_Hello')->hmvc_page(), // Call mvc_page method from Model_Hello
-			'date' => Carbon::now() // Instantiate today date with Carbon
+			'welcome' => Load::model('Model_Welcome')->welcome(), // Call the my_name method from Model_Welcome
+			'hmvc_page' => Load::model('Homepage\Model_Hello')->hmvc_page(), // Call the mvc_page method from Model_Hello inside the Homepage module
+			'date' => Carbon::now() // Instantiate today's date with Carbon
 		];
 
 		// Load HMVC view page
