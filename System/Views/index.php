c7ca14aebd7ae7b580672c4d9ad593b3afb91e9a
kazuyamarino <vikry.yuansah@gmail.com>
2022-02-08 16:01:34 +0700

    change index.php information && update version

--- System/Views/index.php
+++ System/Views/index.php
@@ -16,91 +16,12 @@
 				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#nsy-features-"><strong>NSY Features</strong></a>&nbsp;<&nbsp;<code>Features available in NSY.</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/USERGUIDE.md"><strong>NSY User Guide</strong></a>&nbsp;<&nbsp;<code>The guide before starting to use NSY.</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#user-guide-"><strong>NSY User Guide</strong></a>&nbsp;<&nbsp;<code>The guide before starting to use NSY.</code></h4>
+			</div>
+			<div>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#system-guide-"><strong>NSY Sistem Guide</strong></a>&nbsp;<&nbsp;<code>The guide system.</code></h4>
 			</div>
 		</div>
 
-		<hr>
-
-		<div>
-			<h2><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#nsy-features-">NSY Features :</a></h2>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/USERGUIDE.md#composer-on-nsy-framework">Composer on NSY</a>&nbsp;<&nbsp;<code>NSY support Composer for install/update libraries.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/USERGUIDE.md#framework-configuration">Framework Configuration</a>&nbsp;<&nbsp;<code>Simple config.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/USERGUIDE.md#helpers">NSY Helper</a>&nbsp;<&nbsp;<code>Creating custom methods with ease.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/USERGUIDE.md#routes">NSY Routing System</a>&nbsp;<&nbsp;<code>The Routes.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/USERGUIDE.md#mvc--hmvc">MVC & HMVC</a>&nbsp;<&nbsp;<code>The design pattern.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/USERGUIDE.md#introducting-to-nsy-assets-manager">NSY Assets Manager</a>&nbsp;<&nbsp;<code>Introducting to NSY Assets Manager.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/USERGUIDE.md#psr-4-autoloading">PSR-4 Autoloading</a>&nbsp;<&nbsp;<code>See explanation.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/USERGUIDE.md#nsy-cli-command-line">NSY CLI</a>&nbsp;<&nbsp;<code>A commands to facilitate users in operating NSY.</code></h4>
-			</div>
-
-			<hr>
-
-			<h2><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/SYSGUIDE_1.md#usefull-method">NSY Useful Library :</a></h2>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/SYSGUIDE_1.md#security-helper">Anti XSS & CSRF Token</a>&nbsp;<&nbsp;<code>A Security helper method.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/SYSGUIDE_2.md#primary--secondary-database-connections">Primary and Secondary Database Connection</a>&nbsp;<&nbsp;<code>Database connection.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/SYSGUIDE_1.md#generate-random-number">Generate Number</a>&nbsp;<&nbsp;<code>To generate a sequence of random numbers that correspond to the desired number or prefix.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/SYSGUIDE_1.md#php-session">Session Library</a>&nbsp;<&nbsp;<code>PHP library for handling session.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/SYSGUIDE_1.md#specify-an-empty-variable-or-not">Variable Checking</a>&nbsp;<&nbsp;<code>Checking the empty variable or not.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/SYSGUIDE_1.md#aurora-file-export">Aurora File Export</a>&nbsp;<&nbsp;<code>Aurora library.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/SYSGUIDE_1.md#cookie-library">Cookie Library</a>&nbsp;<&nbsp;<code>PHP library for handling cookies.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/SYSGUIDE_1.md#nsy-ftp-client-library">FTP Client</a>&nbsp;<&nbsp;<code>A flexible FTP and SSL-FTP client for PHP.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/SYSGUIDE_1.md#imageresize-library">ImageResize Library</a>&nbsp;<&nbsp;<code>PHP library to resize, scale and crop images.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://carbon.nesbot.com/docs/">Carbon DateTime</a>&nbsp;<&nbsp;<code>Simple PHP API extension for DateTime.</code></h4>
-			</div>
-
-			<hr>
-
-			<h2><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/SYSGUIDE_2.md#nsy-system-guide-part-2">NSY System Guide :</a></h2>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/SYSGUIDE_2.md#nsy-migrations">NSY Database Migrations</a>&nbsp;<&nbsp;<code>Simple database migration.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/SYSGUIDE_2.md#the-controllers">The NSY Controllers</a>&nbsp;<&nbsp;<code>NSY controller system.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/SYSGUIDE_2.md#the-models">The NSY Models</a>&nbsp;<&nbsp;<code>NSY model system.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/SYSGUIDE_2.md#razr---the-powerful-php-template-engine">Razr Template Engine</a>&nbsp;<&nbsp;<code>Razr - The powerful PHP template engine.</code></h4>
-			</div>
-			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/SYSGUIDE_1.md"><strong>NSY System Guide Part 1</strong></a>&nbsp;&amp;&nbsp;<a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/SYSGUIDE_2.md"><strong>NSY System Guide Part 2</strong></a>&nbsp;<&nbsp;<code>The guide for starting to use NSY System.</code></h4>
-			</div>
-		</div>
 	</div>
 </div>
