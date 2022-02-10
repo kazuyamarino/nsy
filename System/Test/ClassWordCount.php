26a932f3a5e249e8c05b9b7ccce3c28ffd7b2a9e
kazuyamarino <vikry.yuansah@gmail.com>
2022-02-10 15:49:08 +0700

    example phpunit testing

---
+++ System/Test/ClassWordCount.php
@@ -0,0 +1,8 @@
+<?php
+class ClassWordCount
+{
+    public function countWords($sentence)
+    {
+        return count(explode(" ",$sentence));
+    }
+}
---
+++ System/Test/SimpleTest.php
@@ -0,0 +1,17 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+require_once "ClassWordCount.php";
+
+class SimpleTest extends TestCase
+{
+    public function testCountWords()
+    {
+        $Wc = new ClassWordCount();
+
+        $TestSentence = "My name is NSY"; // 4 Kata ..
+        $WordCount = $Wc->countWords($TestSentence);
+
+        $this->assertEquals(4, $WordCount);
+    }
+}
