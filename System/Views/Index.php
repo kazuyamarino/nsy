c979f8a05af3e98d0aa4086af2fe84168795ed48
kazuyamarino <vikry.yuansah@gmail.com>
2023-03-31 14:12:11 +0700

    update information index.php

--- System/Views/Index.php
+++ System/Views/Index.php
@@ -13,13 +13,31 @@
 				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#crud-example"><strong>Shyffon & Vylma</strong></a>&nbsp;<&nbsp;<code>The CRUD example build by NSY.</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#nsy-features-"><strong>NSY Features</strong></a>&nbsp;<&nbsp;<code>Features available in NSY.</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#features-"><strong>NSY Features</strong></a>&nbsp;<&nbsp;<code>Features available in NSY.</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#user-guide-"><strong>NSY User Guide</strong></a>&nbsp;<&nbsp;<code>The guide before starting to use NSY.</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#nsy-helper-function-"><strong>NSY Helper Function</strong></a>&nbsp;<&nbsp;<code>List of helper functions in NSY</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#system-guide-"><strong>NSY Sistem Guide</strong></a>&nbsp;<&nbsp;<code>The guide system.</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#external-helper-function-"><strong>External Helper Function</strong></a>&nbsp;<&nbsp;<code>List of external helper functions in NSY</code></h4>
+			</div>
+			<div>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#nsy-middleware"><strong>NSY Middleware</strong></a>&nbsp;<&nbsp;<code>NSY has support middleware</code></h4>
+			</div>
+			<div>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#nsy-migration"><strong>NSY Migration</strong></a>&nbsp;<&nbsp;<code>Migration is like version control for your database</code></h4>
+			</div>
+			<div>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#razr-template-engine"><strong>Razr Template Engine</strong></a>&nbsp;<&nbsp;<code>Razr is a powerful PHP template engine for PHP</code></h4>
+			</div>
+			<div>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#nsy-security-function"><strong>NSY Security Function</strong></a>&nbsp;<&nbsp;<code>NSY provides security functions including antiXSS and CSRF</code></h4>
+			</div>
+			<div>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#nsy-controller"><strong>NSY Controller</strong></a>&nbsp;<&nbsp;<code>Controllers act as an interface between Model and View components</code></h4>
+			</div>
+			<div>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#nsy-model"><strong>NSY Model</strong></a>&nbsp;<&nbsp;<code>The Model component corresponds to all the data-related logic that the user works with</code></h4>
 			</div>
 		</div>
 
