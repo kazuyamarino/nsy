25f72aad163de2515b8062adbf3a22dc58c05e9b
kazuyamarino <vikry.yuansah@gmail.com>
2023-06-08 23:46:34 +0700

    update link info index.php

--- System/Views/Index.php
+++ System/Views/Index.php
@@ -13,31 +13,31 @@
 				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#crud-example"><strong>Shyffon & Vylma</strong></a>&nbsp;<&nbsp;<code>The CRUD example build by NSY.</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#features-"><strong>NSY Features</strong></a>&nbsp;<&nbsp;<code>Features available in NSY.</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#features"><strong>NSY Features</strong></a>&nbsp;<&nbsp;<code>Features available in NSY.</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#nsy-helper-function-"><strong>NSY Helper Function</strong></a>&nbsp;<&nbsp;<code>List of helper functions in NSY</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#nsy-helper-function"><strong>NSY Helper Function</strong></a>&nbsp;<&nbsp;<code>List of helper functions in NSY</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#external-helper-function-"><strong>External Helper Function</strong></a>&nbsp;<&nbsp;<code>List of external helper functions in NSY</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#external-helper-function"><strong>External Helper Function</strong></a>&nbsp;<&nbsp;<code>List of external helper functions in NSY</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#nsy-middleware"><strong>NSY Middleware</strong></a>&nbsp;<&nbsp;<code>NSY has support middleware</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#nsy-middleware"><strong>NSY Middleware</strong></a>&nbsp;<&nbsp;<code>NSY has support middleware</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#nsy-migration"><strong>NSY Migration</strong></a>&nbsp;<&nbsp;<code>Migration is like version control for your database</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#nsy-migration"><strong>NSY Migration</strong></a>&nbsp;<&nbsp;<code>Migration is like version control for your database</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#razr-template-engine"><strong>Razr Template Engine</strong></a>&nbsp;<&nbsp;<code>Razr is a powerful PHP template engine for PHP</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#razr-template-engine"><strong>Razr Template Engine</strong></a>&nbsp;<&nbsp;<code>Razr is a powerful PHP template engine for PHP</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#nsy-security-function"><strong>NSY Security Function</strong></a>&nbsp;<&nbsp;<code>NSY provides security functions including antiXSS and CSRF</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#nsy-security-function"><strong>NSY Security Function</strong></a>&nbsp;<&nbsp;<code>NSY provides security functions including antiXSS and CSRF</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#nsy-controller"><strong>NSY Controller</strong></a>&nbsp;<&nbsp;<code>Controllers act as an interface between Model and View components</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#nsy-controller"><strong>NSY Controller</strong></a>&nbsp;<&nbsp;<code>Controllers act as an interface between Model and View components</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs#nsy-model"><strong>NSY Model</strong></a>&nbsp;<&nbsp;<code>The Model component corresponds to all the data-related logic that the user works with</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy-docs/blob/master/README.md#nsy-model"><strong>NSY Model</strong></a>&nbsp;<&nbsp;<code>The Model component corresponds to all the data-related logic that the user works with</code></h4>
 			</div>
 		</div>
 
