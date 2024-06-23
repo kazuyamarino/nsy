e07e74075a7c2691b25a8c36a9ed9cdb3b3ebdf7
kazuyamarino <vikry.yuansah@gmail.com>
2024-06-23 20:28:48 +0700

    auto setup directory when run nsy --setup

--- .cli/nsy.settingup.sh
+++ .cli/nsy.settingup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 run_setup() {
-	echo "Enter directory name > "
-	read response
-	if [ -n "$response" ];
-	then
-	    filename=$response
+	current_dir=$(pwd)
+	last_dir_name=$(basename "$current_dir")
 
+	echo "Prepare for NSY settings..."
+	echo "The current working directory is: $last_dir_name"
+	sleep 3
+
+	if [ -n "$last_dir_name" ];
+	then
 		if [ ! -e ./env.php ]
 		then
 			cp docs/apache/for_public/.htaccess ./public/.htaccess
@@ -14,19 +17,19 @@ run_setup() {
 			cp .cli/tmp/env.example.php docs/env.example/env.example.php
 			cp .cli/tmp/system.js ./public/assets/js/config/system.js
 			cp .cli/tmp/default ./docs/nginx/sites-enabled/default
-			sed -i "s/nsy/$filename/g" ./env.php
-			sed -i "s/nsy/$filename/g" ./docs/env.example/env.example.php
-			sed -i "s/nsy/$filename/g" ./public/assets/js/config/system.js
-			sed -i "s/nsy/$filename/g" ./docs/nginx/sites-enabled/default
+			sed -i "s/nsy/$last_dir_name/g" ./env.php
+			sed -i "s/nsy/$last_dir_name/g" ./docs/env.example/env.example.php
+			sed -i "s/nsy/$last_dir_name/g" ./public/assets/js/config/system.js
+			sed -i "s/nsy/$last_dir_name/g" ./docs/nginx/sites-enabled/default
 
 			printf "Please wait...\n"
 			sleep 3
 
-			printf "NSY successfully prepared\n"
+			printf "NSY has been set up\n"
 		else
-		    printf "NSY already prepared\n"
+			printf "NSY has already been prepared\n"
 		fi
 	else
-		printf "Application directory name undefined\n"
+		printf "The application directory name is not specified\n"
 	fi
 }
