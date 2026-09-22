74f041610d2736a5105f307acde7299be60b738e
kazuyamarino <vikry.yuansah@gmail.com>
2026-09-22 16:57:11 +0700

    refactor: point Index_Welcome docs links to GitHub blob/master/docs/README_*.md

--- System/Apps/General/Views/Index_Welcome.php
+++ System/Apps/General/Views/Index_Welcome.php
@@ -7,28 +7,28 @@
 		<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy/releases"><strong>Release Version&nbsp;</strong></a>|<a target="_blank" href="https://github.com/kazuyamarino/nsy"><strong>&nbsp;View On Github&nbsp;</strong></a></h4>
 		<div>
 			<div>
-				<h4><a target="_blank" href="@( base_url() )docs/README_LIBRARIES.md"><strong>NSY Libraries</strong></a>&nbsp;<&nbsp;<code>File, LanguageCode, Query Builder (qb), Validate</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy/blob/master/docs/README_LIBRARIES.md"><strong>NSY Libraries</strong></a>&nbsp;<&nbsp;<code>File, LanguageCode, Query Builder (qb), Validate</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="@( base_url() )docs/README_LOAD_AND_ASSETMANAGER.md"><strong>Load & Asset Manager</strong></a>&nbsp;<&nbsp;<code>Load::view/template/model & Add::link/script (?v=filemtime)</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy/blob/master/docs/README_LOAD_AND_ASSETMANAGER.md"><strong>Load & Asset Manager</strong></a>&nbsp;<&nbsp;<code>Load::view/template/model & Add::link/script (?v=filemtime)</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="@( base_url() )docs/README_HELPERS_GLOBAL.md"><strong>Global Helpers</strong></a>&nbsp;<&nbsp;<code>base_url(), is_filled(), css_url()</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy/blob/master/docs/README_HELPERS_GLOBAL.md"><strong>Global Helpers</strong></a>&nbsp;<&nbsp;<code>base_url(), is_filled(), css_url()</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="@( base_url() )docs/README_NSY_ROUTER.md"><strong>NSY Router</strong></a>&nbsp;<&nbsp;<code>Route::get/post/group + RouteExample.php 14 sections</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy/blob/master/docs/README_NSY_ROUTER.md"><strong>NSY Router</strong></a>&nbsp;<&nbsp;<code>Route::get/post/group + RouteExample.php 14 sections</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="@( base_url() )docs/README_MIGRATION.md"><strong>NSY Migration</strong></a>&nbsp;<&nbsp;<code>Mig::create_table() DRY (quoteIdent/execDDL)</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy/blob/master/docs/README_MIGRATION.md"><strong>NSY Migration</strong></a>&nbsp;<&nbsp;<code>Mig::create_table() DRY (quoteIdent/execDDL)</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="@( base_url() )docs/README_MODEL.md"><strong>NSY Model & DB</strong></a>&nbsp;<&nbsp;<code>DB::query() + NSY_DB::connect() unified</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy/blob/master/docs/README_MODEL.md"><strong>NSY Model & DB</strong></a>&nbsp;<&nbsp;<code>DB::query() + NSY_DB::connect() unified</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="@( base_url() )docs/README_QUERY_BUILDER.md"><strong>Query Builder</strong></a>&nbsp;<&nbsp;<code>qb('users')->whereIn()->paginate() — 1 line</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy/blob/master/docs/README_QUERY_BUILDER.md"><strong>Query Builder</strong></a>&nbsp;<&nbsp;<code>qb('users')->whereIn()->paginate() — 1 line</code></h4>
 			</div>
 			<div>
-				<h4><a target="_blank" href="@( base_url() )docs/OVERVIEW.md"><strong>Overview</strong></a>&nbsp;<&nbsp;<code>Composer, Config (env), Helpers, MVC/HMVC, CLI</code></h4>
+				<h4><a target="_blank" href="https://github.com/kazuyamarino/nsy/blob/master/docs/OVERVIEW.md"><strong>Overview</strong></a>&nbsp;<&nbsp;<code>Composer, Config (env), Helpers, MVC/HMVC, CLI</code></h4>
 			</div>
 		</div>
 
